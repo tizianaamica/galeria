657c576bf54f992938a4a6b965b68f295de20e9e
tizianaamica <tizianaamicarella.tag@gmail.com>
2021-05-25 01:47:54 -0400

    first commit

---
+++ views/index.view.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <head>
+        <div class="contenedor">
+            <h1 class="titulo">Mi Increible Galeria en PHP y MySQL</h1>
+        </div>
+    </head>
+
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/2.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/3.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/4.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/5.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/6.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/7.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="imagenes/8.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina Anterior</a>            
+                <a href="#" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>            
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p class="copyright">Galeria creada por Tiziana Amicarella </p>
+    </footer>
+    
+
+</body>
+
+</html>
